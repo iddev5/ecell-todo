02fb528519f6f745431b1bbb70a14019c30d1a9f
iddev5 <ayushbardhan5@gmail.com>
2024-06-04 16:50:22 +0530

    Backend: use cors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db.js";
 import {
@@ -13,6 +14,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.post("/api", createTodo);
