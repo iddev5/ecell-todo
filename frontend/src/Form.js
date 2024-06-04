0db0dd96d3f4f176c05ff6881ae665fed37e9b21
iddev5 <ayushbardhan5@gmail.com>
2024-06-04 18:07:23 +0530

    frontend: Implement todo updating system

--- frontend/src/Form.js
+++ frontend/src/Form.js
@@ -1,10 +1,10 @@
 
 function Form({onSubmit, onCancel, defaultTitle, defaultDesc}) {
 	return <form onSubmit={onSubmit}>
-    <input type="text" name="title" id="title" />
+    <input type="text" name="title" id="title" defaultValue={defaultTitle} />
     <label htmlFor="title">Title</label>
 
-    <textarea name="desc" id="desc" />
+    <textarea name="desc" id="desc" defaultValue={defaultDesc} />
     <label htmlFor="desc">Description</label>
 
     <button type="submit" value="submit">Done</button>
--- frontend/src/Todo.js
+++ frontend/src/Todo.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import Form from "./Form.js";
 
 function Todo({data, todos, setTodos}) {
 	const [change, setChange] = useState(false)
@@ -12,10 +13,37 @@ function Todo({data, todos, setTodos}) {
 		setTodos(newTodos);
 	}
 
+	async function updateTodo(event) {
+		event.preventDefault();
+
+		// TODO: OR-ing is not needed if below TODO is fixed
+		const form_data = {
+			title: event.target.title.value || data.title,
+			desc: event.target.desc.value || data.desc,
+		};
+
+		const res = await fetch(`/api/${data._id}/`, {
+			method: 'PUT',
+			body: JSON.stringify(form_data),
+			headers: {
+				'Content-Type': "application/json"
+			}
+		});
+
+		const idx = todos.findIndex(it => data._id === it._id);
+		const new_todos = todos.slice();
+		// TODO: fix PUT returning old data
+		new_todos[idx].title = form_data.title;
+		new_todos[idx].desc = form_data.desc;
+
+		setTodos(new_todos);
+		setChange(false);
+	}
+
 	return <div>
 			{change &&
 				<div>
-
+					<Form onSubmit={updateTodo} onCancel={() => setChange(false)} defaultTitle={data.title} defaultDesc={data.desc} />
 				</div>}
 			{!change &&
 	      <div>
