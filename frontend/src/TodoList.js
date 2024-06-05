32fce77169edc95e1b5f44c01b4b2b67978583e6
iddev5 <ayushbardhan5@gmail.com>
2024-06-05 16:31:49 +0530

    frontend: remove search form, out of scope for the project
    Maybe I will revisit it later

--- frontend/src/TodoList.js
+++ frontend/src/TodoList.js
@@ -30,29 +30,17 @@ function TodoList({ todos, setTodos, state, setState }) {
     <div className="container">
       <div className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <div>
-            <a className="navbar-brand" href="#">
-              Todo List
-            </a>
-            {!addTodo && (
-              <button
-                className="btn btn-primary"
-                onClick={() => setAddTodo(true)}
-              >
-                New
-              </button>
-            )}
-          </div>
-          <form className="d-flex m-2" role="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
+          <a className="navbar-brand" href="#">
+            Todo List
+          </a>
+          {!addTodo && (
+            <button
+              className="btn btn-primary"
+              onClick={() => setAddTodo(true)}
+            >
+              New
             </button>
-          </form>
+          )}
         </div>
       </div>
       {state === "fetching-get" && <p>Loading data</p>}
