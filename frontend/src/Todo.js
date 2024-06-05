e607cdf6fa3e726bb30425efad3cde078006b34f
iddev5 <ayushbardhan5@gmail.com>
2024-06-05 16:29:38 +0530

    frontend: strikethrough text when completed and replaced all icons with bootstrap icons

--- frontend/src/Todo.js
+++ frontend/src/Todo.js
@@ -70,34 +70,85 @@ function Todo({ data, todos, setTodos }) {
       {!change && (
         <div className="container">
           <div className="row">
-            <button
-              className="col-1 p-0"
-              onClick={markCompleted}
-              checked
-            ></button>
-            <button
-              ref={titleRef}
-              className="col text-start border-0 bg-transparent"
-              onClick={() => setChange(true)}
-            >
-              <h1>{data.title}</h1>
+            <button className="col-1 p-0 btn" onClick={markCompleted} checked>
+              {complete && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  class="bi bi-check-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                  <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05" />
+                </svg>
+              )}
+              {!complete && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  class="bi bi-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                </svg>
+              )}
             </button>
             <button
-              className="col-1 btn bg-danger p-2 btn-close opacity-100"
+              ref={titleRef}
+              className="col fs-4 text-start border-0 bg-transparent"
+              style={
+                complete
+                  ? {
+                      "text-decoration": "line-through",
+                      "text-decoration-thickness": "2px",
+                      "font-style": "italic",
+                    }
+                  : {}
+              }
+              onClick={() => setChange(true)}
+            >
+              {data.title}
+            </button>
+            <button
+              className="col-1 btn rounded-circle text-danger p-2 opacity-100"
               onClick={deleteTodo}
-            ></button>
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                class="bi bi-trash"
+                viewBox="0 0 16 16"
+              >
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+              </svg>
+            </button>
           </div>
           <div className="row">
             <div className="col-1"></div>
             <button
               ref={descRef}
               className="col text-start border-0 bg-transparent"
+              style={
+                complete
+                  ? {
+                      "text-decoration": "line-through",
+                      "text-decoration-thickness": "2px",
+                      "font-style": "italic",
+                    }
+                  : {}
+              }
               onClick={() => setChange(true)}
             >
-              <p>{data.desc}</p>
+              {data.desc}
             </button>
           </div>
-          {complete && <p>This is complete</p>}
         </div>
       )}
     </div>
