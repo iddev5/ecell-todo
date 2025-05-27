ad9c5e4460c26a7c11ae9ca4671368ade9fd722c
iddev5 <ayushbardhan5@gmail.com>
2025-05-27 21:43:40 +0530

    frontend: Use lucide-react for all icons

--- frontend/src/Todo.js
+++ frontend/src/Todo.js
@@ -1,6 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import Form from "./Form.js";
 import { useDispatch } from "react-redux";
+import { Trash2, Circle, CircleCheckBig } from "lucide-react";
 import api from "./api.js";
 
 function Todo({ data }) {
@@ -96,7 +97,12 @@ function Todo({ data }) {
       {!onUpdate && (
         <div className="container">
           <div className="row">
-            <button className="col-1 p-0 btn" onClick={markCompletedCb} checked>
+            <button
+              className="col-1 p-0 btn"
+              onClick={markCompletedCb}
+              style={{ border: "none" }}
+              checked
+            >
               {onComplete && (
                 <div className="d-flex justify-content-center">
                   <div className="text-center">
@@ -108,31 +114,8 @@ function Todo({ data }) {
                   </div>
                 </div>
               )}
-              {complete && !onComplete && (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="currentColor"
-                  className="bi bi-check-circle"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                  <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05" />
-                </svg>
-              )}
-              {!complete && !onComplete && (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="currentColor"
-                  className="bi bi-circle"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                </svg>
-              )}
+              {complete && !onComplete && <CircleCheckBig />}
+              {!complete && !onComplete && <Circle />}
             </button>
             <button
               ref={titleRef}
@@ -154,18 +137,9 @@ function Todo({ data }) {
             <button
               className="col-1 btn text-danger p-2 opacity-100"
               onClick={deleteTodoCb}
+              style={{ border: "none" }}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="currentColor"
-                className="bi bi-trash"
-                viewBox="0 0 16 16"
-              >
-                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-              </svg>
+              <Trash2 />
             </button>
           </div>
           <div className="row">
--- frontend/src/TodoList.js
+++ frontend/src/TodoList.js
@@ -1,4 +1,5 @@
 import Todo from "./Todo.js";
+import { FileText } from "lucide-react";
 
 function TodoList({ todos, state }) {
   return (
@@ -22,17 +23,7 @@ function TodoList({ todos, state }) {
         ))}
         {todos.length === 0 && state !== "fetching-get" && (
           <div className="text-center" style={{ "margin-top": "10em" }}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="64"
-              height="64"
-              fill="currentColor"
-              class="bi bi-file-earmark-text"
-              viewBox="0 0 16 16"
-            >
-              <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5" />
-              <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
-            </svg>
+            <FileText size={64} />
             <h2>No todos found...</h2>
             <p>Create a task to get started!</p>
           </div>
