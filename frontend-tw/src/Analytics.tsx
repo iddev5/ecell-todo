996c8108486baa2972bfde68bff5b9dc3b71c920
iddev5 <ayushbardhan5@gmail.com>
2025-05-29 16:16:54 +0530

    frontend-tw: Analytics page top bar

--- frontend-tw/src/Analytics.tsx
+++ frontend-tw/src/Analytics.tsx
@@ -1,6 +1,9 @@
 import Header from "./sections/Header";
-import { CircleCheckBig, Clock, Gauge, RotateCcw } from "lucide-react";
+import { ChevronLeft, CircleCheckBig, Clock, Gauge, RotateCcw } from "lucide-react";
+import { Button } from "./components/ui/button";
 import React from 'react';
+import { ToggleGroup, ToggleGroupItem } from "./components/ui/toggle-group";
+import { Bold, Italic, Underline } from "lucide-react"
 
 function StatCard(props: {count: string, name: string, icon: React.ReactNode}) {
     return (
@@ -24,6 +27,22 @@ export default function Analytics() {
 
         <section className="w-full flex justify-center overflow-hidden">
             <div className="sm:w-[40vw] w-screen min-h-screen h-full pb-26 shadow-lg">
+                <div className="flex justify-between items-center m-4 mt-5">
+                    <div className="flex items-center gap-5">
+                        <Button variant='ghost'>
+                            {/* NOTE: size is not supported for icons inside <Button> */}
+                            <ChevronLeft style={{ scale: 2 }} />
+                        </Button>
+                        <p className='text-3xl'>Analytics</p>
+                    </div>
+                    <ToggleGroup type='single' variant='outline' className="mr-4">
+                        <ToggleGroupItem value='1d'>1D</ToggleGroupItem>
+                        <ToggleGroupItem value='1w'>1W</ToggleGroupItem>
+                        <ToggleGroupItem value='2w'>2W</ToggleGroupItem>
+                        <ToggleGroupItem value='1m'>1M</ToggleGroupItem>
+                        <ToggleGroupItem value='6m'>6M</ToggleGroupItem>
+                    </ToggleGroup>
+                </div>
                 <div className="flex border-y-1 mt-4">
                     <StatCard count="92%" name="Compl. Rate" icon={<CircleCheckBig size={16} />} />
                     <StatCard count="4h" name="Avg. Time" icon={<Clock size={16} />} />
