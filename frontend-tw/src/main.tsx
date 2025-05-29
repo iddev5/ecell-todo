4826b113649ce2aa36e5684681d91643da68dc7f
iddev5 <ayushbardhan5@gmail.com>
2025-05-29 18:28:27 +0530

    frontend-tw: Added theme switcher

--- frontend-tw/src/main.tsx
+++ frontend-tw/src/main.tsx
@@ -6,16 +6,19 @@ import Analytics from './Analytics.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router';
 import { Provider } from 'react-redux'
 import store from './lib/store.ts'
+import { ThemeProvider } from './components/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/analytics" element={<Analytics />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <ThemeProvider defaultTheme='light' storageKey='flowcode-ui-theme'>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/analytics" element={<Analytics />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </ThemeProvider>
   </StrictMode>,
 )
--- frontend-tw/src/sections/Header.tsx
+++ frontend-tw/src/sections/Header.tsx
@@ -1,12 +1,26 @@
 
+import { Switch } from "@/components/ui/switch";
 import { Input } from "../components/ui/input";
+import { useTheme } from '@/components/theme-provider';
+import { Moon, Sun } from 'lucide-react';
 
 export default function Header() {
+    const { theme, setTheme } = useTheme();
+
+    const changeTheme = (b: boolean) => {
+        setTheme(b ? 'dark' : 'light');
+    }
+
     return (
         <header className="w-full flex justify-center shadow-md">
             <div className="w-[40vw] flex justify-between p-4 px-8 shadow-lg">
                 <p className="text-3xl font-semibold">Justflow</p>
-                <Input className="w-[20vw]" placeholder="Search tasks..." />
+                <div className="flex items-center gap-2">
+                    <Input className="w-[20vw]" placeholder="Search tasks..." />
+                    <Switch onCheckedChange={changeTheme} />
+                    {theme === 'light' && <Sun />}
+                    {theme === 'dark' && <Moon />}
+                </div>
             </div>
         </header>
     );
