7a7de0f19c014f1cb2cfdb40fba5dccb75c7427e
iddev5 <ayushbardhan5@gmail.com>
2025-05-31 12:24:18 +0530

    frontend-tw: Move theme change inside side bar

--- frontend-tw/src/sections/Header.tsx
+++ frontend-tw/src/sections/Header.tsx
@@ -6,7 +6,7 @@ import { Moon, Search, Sun } from 'lucide-react';
 import { auth } from '../lib/firebase';
 import { useAuth } from "@/components/auth-provider";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle, SheetFooter } from "@/components/ui/sheet";
 
 function ProfileTrigger() {
     const { user } = useAuth();
@@ -34,9 +34,14 @@ function ProfileTrigger() {
 
 function SideBar() {
     const { user } = useAuth();
+    const { theme, setTheme } = useTheme();
 
     const userLogOut = async () => {
         await auth.signOut();
+    }
+
+    const changeTheme = (b: boolean) => {
+        setTheme(b ? 'dark' : 'light');
     }
 
     return (
@@ -48,17 +53,18 @@ function SideBar() {
             <Button onClick={userLogOut}>
                 Log out
             </Button>
+            <SheetFooter>
+                <div className="flex gap-2 items-center">
+                    {theme === 'light' && <Sun />}
+                    {theme === 'dark' && <Moon />}
+                    <Switch onCheckedChange={changeTheme} />
+                </div>
+            </SheetFooter>
         </SheetHeader>
     );
 }
 
 export default function Header() {
-    const { theme, setTheme } = useTheme();
-
-    const changeTheme = (b: boolean) => {
-        setTheme(b ? 'dark' : 'light');
-    }
-
     return (
         <header className="w-full flex justify-center shadow-md">
             <div className="w-[40vw] flex justify-between p-4 px-8 shadow-lg">
@@ -75,9 +81,6 @@ export default function Header() {
                         <Input className="w-[20vw]" placeholder="Search tasks..." />
                         <Search className="absolute top-1/2 -translate-y-1/2 right-2 border-1 rounded-md p-1 opacity-80" />
                     </div>
-                    <Switch onCheckedChange={changeTheme} />
-                    {theme === 'light' && <Sun />}
-                    {theme === 'dark' && <Moon />}
                 </div>
             </div>
         </header>
